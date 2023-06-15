e155f0f87910aeba85bbfc0120852ad74e08a90f
vipinkaulwar13 <100561500+vipinkaulwar13@users.noreply.github.com>
2023-06-15 18:59:08 +0530

    Time: 0 ms (100.00%), Space: 20.7 MB (27.27%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word, ans;
+        while(ss>>word){
+            ans = word + " " + ans;
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
