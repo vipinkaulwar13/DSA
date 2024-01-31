cadf6a06ae8aa58aa691ff0b3b5c21b9e39f6034
vipinkaulwar13 <100561500+vipinkaulwar13@users.noreply.github.com>
2024-01-31 19:08:34 +0530

    Time: 4 ms (30.14%), Space: 9 MB (66.50%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    
-    int solve(vector<int>& nums, int x, vector<int>& dp){
-        if(x<0){
-            return 0;
-        }
-        if(x == 0){
-            return nums[x];
-        }
-        if(dp[x] != -1){
-            return dp[x];
+    int solve(vector<int>& nums, int x){
+        
+        if(x == 1){
+            return nums[0];
         }
         
-        int include = solve(nums, x-2, dp) + nums[x];
-        int exclude = solve(nums, x-1, dp);
+        int prev1 = 0;
+        int prev2 = nums[0];
         
-        dp[x] = max(include, exclude);
-        
-        return dp[x];
+        for(int i = 1; i<x; i++){
+            int include = prev1 + nums[i];
+            int exclude = prev2;
+            
+            int ans = max(include, exclude);
+            prev1 = prev2;
+            prev2 = ans;
+            
+        }
+        return prev2;
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, -1);
-        int ans = solve(nums, n-1, dp);
-        return ans;
+        return solve(nums, n);
     }
 };
