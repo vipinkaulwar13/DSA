2fa72529ab30b0a34316b30392e9f3fdc92f4fc5
vipinkaulwar13 <100561500+vipinkaulwar13@users.noreply.github.com>
2024-06-19 10:23:08 +0530

    Time: 50 ms (59.90%), Space: 41.2 MB (57.58%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
-//     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-//         vector<pair<int, int>>dp;
-//         int ans = 0;
-//         for(int i = 0; i<profit.size(); i++){
-//             dp.push_back({profit[i], difficulty[i]});
-//         }
-        
-//         sort(dp.begin(), dp.end());
-//         sort(worker.begin(), worker.end());
-        
-//         int j = dp.size()-1;
-//         int i = worker.size()-1;
-//         while(i>0 && j>0){
-//             if(worker[i]>=dp[j].second){
-//                 ans+= dp[j].first;
-//                 i--;
-//             }
-//             else{
-//                 j--;
-//             }
-//         }
-//         return ans;
-//         //theory 1- use upper bound and lower bound to find workers who can work on jobs which have a difficulty <= the capacity of a worker.
-//         // insert the diff and pro vectors in a vector pair and sort them
-        
-//     }
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        vector<pair<int, int>> jobs;
-        int N = profit.size(), res = 0, i = 0, best = 0;
-        for (int j = 0; j < N; ++j)
-            jobs.push_back(make_pair(difficulty[j], profit[j]));
-        sort(jobs.begin(), jobs.end());
-        sort(worker.begin(), worker.end());
-        for (int & ability : worker) {
-            while (i < N && ability >= jobs[i].first)
-                best = max(jobs[i++].second, best);
-            res += best;
+        vector<pair<int, int>>dp;
+        int ans = 0;
+        for(int i = 0; i<profit.size(); i++){
+            dp.push_back({profit[i], difficulty[i]});
+            //dp.emplace_back(profit[i], difficulty[i]);
         }
-        return res;
+        
+        sort(dp.begin(), dp.end());
+        sort(worker.begin(), worker.end());
+        
+        int j = dp.size()-1;
+        int i = worker.size()-1;
+        while(i>=0 && j>=0){
+            if(worker[i]>=dp[j].second){
+                ans+= dp[j].first;
+                i--;
+            }
+            else{
+                j--;
+            }
+        }
+        return ans;
+        //theory 1- use upper bound and lower bound to find workers who can work on jobs which have a difficulty <= the capacity of a worker.
+        // insert the diff and pro vectors in a vector pair and sort them
+        
     }
 };
