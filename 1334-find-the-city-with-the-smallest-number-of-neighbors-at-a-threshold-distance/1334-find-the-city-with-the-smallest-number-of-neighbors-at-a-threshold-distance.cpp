eb3700858ccb776bd470deafb5961139041fac0e
vipinkaulwar13 <100561500+vipinkaulwar13@users.noreply.github.com>
2024-07-28 11:45:34 +0530

    Time: 20 ms (89.15%), Space: 15.5 MB (81.35%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
-   
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> dis(n, vector(n, 10001));
-        int res = 0, smallest = n;
-        for (auto& e : edges)
-            dis[e[0]][e[1]] = dis[e[1]][e[0]] = e[2];
-        for (int i = 0; i < n; ++i)
-            dis[i][i] = 0;
-        for (int k = 0; k < n; ++k)
-            for (int i = 0; i < n; ++i)
-                for (int j = 0; j < n; ++j)
-                    dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; ++j)
-                if (dis[i][j] <= distanceThreshold)
-                    ++count;
-            if (count <= smallest) {
-                res = i;
-                smallest = count;
+        vector<vector<int>>res(n, vector<int>(n, 10001));
+        int small = n, ans = 0;
+        for(int i = 0; i<edges.size(); i++){
+            res[edges[i][0]][edges[i][1]] = edges[i][2];
+            res[edges[i][1]][edges[i][0]] = edges[i][2];
+        }
+        for(int i = 0; i<n; i++){
+            res[i][i] = 0;
+        }
+        for(int v = 0; v<n; v++){
+            for(int i = 0; i<n; i++){
+                for(int j = 0; j<n; j++){
+                    res[i][j] = min(res[i][j], res[i][v]+res[v][j]);
+                }
             }
         }
-        return res;
+        
+        for(int i = 0; i<n; i++){
+            int count = 0;
+            for(int j = 0; j<n; j++){
+                if(res[i][j]<=distanceThreshold){
+                    count++;
+                }
+            }
+            if(count<=small){
+                small = count;
+                ans = i;
+            }
+        }
+        
+        return ans;
     }
 };
