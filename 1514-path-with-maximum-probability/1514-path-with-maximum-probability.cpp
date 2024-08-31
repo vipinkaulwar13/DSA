509fdca180c95b260cc390b276d8fd5b14e19891
vipinkaulwar13 <100561500+vipinkaulwar13@users.noreply.github.com>
2024-08-31 15:58:22 +0530

    Time: 125 ms (51.42%), Space: 70.6 MB (31.06%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        for(int i = 0; i<succProb.size(); i++){
-            succProb[i] = -log(succProb[i]);
+        vector<double> prob(n, 1e9);
+        prob[start_node] = 0;
+        vector<vector<pair<int, double>>>adjlist(n);
+        for(int i = 0; i<edges.size(); i++){
+            adjlist[edges[i][0]].push_back({edges[i][1], -log(succProb[i])});
+            adjlist[edges[i][1]].push_back({edges[i][0], -log(succProb[i])});
         }
+        priority_queue<pair<double,int>, vector<pair<double,int>>, greater<pair<double,int>>>pq;
         
-        vector<double>dist(n, 1e9);
-        dist[start_node] = 0;
-        for(int i = 0; i<n-1; i++){
-            bool updated = false;
-            for(int j = 0 ; j<edges.size(); j++){
-                if(dist[edges[j][0]]+succProb[j]<dist[edges[j][1]]){
-                    dist[edges[j][1]] = dist[edges[j][0]]+succProb[j];
-                    updated = true;
-                }
-                if(dist[edges[j][1]]+succProb[j]<dist[edges[j][0]]){
-                    dist[edges[j][0]] = dist[edges[j][1]]+succProb[j];
-                    updated = true;
+        pq.push({0, start_node});
+        
+        while(!pq.empty()){
+            double dis = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            if(node == end_node){
+                return exp(-dis);
+            }
+            for(auto it :adjlist[node]){
+                if(dis+it.second<prob[it.first]){
+                    prob[it.first] = dis+it.second;
+                    pq.push({dis+it.second, it.first});
                 }
             }
-            if(!updated) break;
         }
-        return exp(-dist[end_node]);
+        return 0.0;
     }
 };
 
