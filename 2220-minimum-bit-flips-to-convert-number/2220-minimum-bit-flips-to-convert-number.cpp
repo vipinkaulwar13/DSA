d24dfcddbe3bee22f4fe8937eae48f0137213f3a
vipinkaulwar13 <100561500+vipinkaulwar13@users.noreply.github.com>
2024-09-11 11:50:44 +0530

    Time: 3 ms (41.47%), Space: 7.6 MB (44.87%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int temp = start^goal;
         int count = 0;
         for(int i = 0; i<32; i++){
-            if(temp&(1<<i)) count++;
+            if((start&1) != (goal&1)){
+                count++;
+            }
+            start >>= 1;
+            goal >>= 1;
         }
         return count;
     }
 };
+
